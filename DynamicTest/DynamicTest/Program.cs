a41cb32eeea86db6ecc617cf78b8d5514896ae91
Mohammad Habibollahzadeh <78mhaz78@gmail.com>
2020-11-02 15:35:03 +0330

    just for test

---
+++ DynamicTest/DynamicTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic person = new DynamicDictionary();
+
+            // Adding new dynamic properties.
+            // The TrySetMember method is called.
+            person.FirstName = "Ellen";
+            person.LastName = "Adams";
+            person.Age = 15;
+
+            // Getting values of the dynamic properties.
+            // The TryGetMember method is called.
+            // Note that property names are case-insensitive.
+            Console.WriteLine(person.firstname + " " + person.lastname +"-----"+person.Age);
+
+            // Getting the value of the Count property.
+            // The TryGetMember is not called,
+            // because the property is defined in the class.
+            Console.WriteLine(
+                "Number of dynamic properties:" + person.Count);
+
+            var config = new DbMigrationsConfiguration<TestDB> { AutomaticMigrationsEnabled = true };
+            var migrator = new DbMigrator(config);
+            migrator.Update();
+
+        }
+    }
+}
---
+++ DynamicTest/DynamicTest/TestDB.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Activities.Statements;
+
+namespace DynamicTest
+{
+    class TestDB : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            var entityMethod = typeof(DbModelBuilder).GetMethod("Entity");
+
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                var entityTypes = assembly
+                  .GetTypes()
+                  .Where(t =>
+                    t.GetCustomAttributes(typeof(PersistentAttribute), inherit: true)
+                    .Any());
+
+                foreach (var type in entityTypes)
+                {
+                    entityMethod.MakeGenericMethod(type)
+                      .Invoke(modelBuilder, new object[] { });
+                }
+            }
+        }
+    }
+    [AttributeUsage(AttributeTargets.Class)]
+    public class PersistentAttribute : Attribute
+    {
+
+    }
+    [Persistent]
+    public class Test
+    {
+        public Test()
+        {
+
+        }
+        public string Name { get; set; }
+        public string family { get; set; }
+        public int ID { get; set; }
+    }
+}
